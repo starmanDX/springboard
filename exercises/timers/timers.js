87e8badaf323caa4df083ccca950e0f2fa5a3856
starmanDX <jamiecordrey@gmail.com>
2020-01-21 23:53:24 -0800

    add initial timer exercise

---
+++ exercises/timers/timers.js
@@ -0,0 +1,11 @@
+//Write a function called countdown that accepts 
+//a number as a parameter and every 1000 milliseconds
+//decrements the value and console.logs it. Once the value
+//is 0 it should log “DONE!” and stop.
+
+
+countDown(4);
+// 3
+// 2
+// 1
+// "DONE!"
