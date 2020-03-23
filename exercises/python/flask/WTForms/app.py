b044b990971053238eb45f58c1ba4e74c6205f52
starmanDX <jamiecordrey@gmail.com>
2020-03-22 19:39:04 -0700

    modify flask WTForms exercise files

--- exercises/python/flask/WTForms/app.py
+++ exercises/python/flask/WTForms/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from werkzeug.utils import secure_filename
 from models import db, connect_db, Pet
-from forms import AddPetForm
+from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
 
@@ -23,10 +23,33 @@ def show_home():
 
 @app.route('/add', methods=['GET', 'POST'])
 def show_add_pet_form():
-    new_pet_form = AddPetForm()
-    if new_pet_form.validate_on_submit():
-        filename = secure_filename(new_pet_form.photo_upload.data.filename)
-        new_pet_form.photo_upload.data.save('static/images/' + filename)
+    form = AddPetForm()
+    if form.validate_on_submit():
+        data = {k:v for k, v in form.data.items() if k != "csrf_token"}
+        
+        new_pet = Pet(**data)
+        db.session.add(new_pet)
+        db.session.commit()
+
+        flash(f"{new_pet.name} has been added to our database!")
+        
         return redirect('/')
     else: 
-        return render_template('add_pet_form.html')
+        return render_template('add_pet_form.html', form = form)
+    
+@app.route('/<int:pet_id>', methods=['GET', 'POST'])
+def edit_add_pet_form(pet_id):
+    form = EditPetForm()
+    pet = Pet.query.get_or_404(pet_id)
+
+    if form.validate_on_submit():
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        pet.photo_url = form.photo_url.data
+        db.session.commit()
+
+        flash(f"{pet.name} has been updated!")
+
+        return redirect('/')
+    else:
+        return render_template('edit_pet_form.html', form=form, pet=pet)
--- exercises/python/flask/WTForms/forms.py
+++ exercises/python/flask/WTForms/forms.py
@@ -1,21 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField, FileField, SelectField, FloatField, IntegerField, BooleanField
-from wtforms.validators import ValidationError, InputRequired, Optional, URL
+from wtforms import BooleanField, StringField, SelectField, IntegerField, TextAreaField
+from wtforms.validators import NumberRange, InputRequired, Optional, URL
 
 
 class AddPetForm(FlaskForm):
     """Form for adding pets."""
 
-    pet_name = StringField("Pet Name", validators=[
+    name = StringField("Pet Name", validators=[
                        InputRequired(message="Pet name can't be blank.")])
-    species = StringField("Species", validators=[
-        InputRequired(message="Species can't be blank.")])
+    species = SelectField("Species", choices=[(None, ''), ('bird', 'Bird'), ('cat', 'Cat'), (
+        'dog', 'Dog'), ('hamster', 'Hamster'), ('reptile', 'Reptile'), ('other', 'Other')], default=None)
     age = IntegerField("Age", validators=[
-        InputRequired(message="Age can't be blank.")])
-    photo_url = StringField("Photo URL", validators=[Optional(), URL(), url_or_upload])
-    photo_upload = FileField("Upload a Photo", validators=[Optional(), url_or_upload])
-    notes = StringField("This is a healthy snack.")
+        InputRequired(message="Age can't be blank."), NumberRange(min=0, max=30, message="Age must be between %(min)s and %(max)s.")])
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+    notes = TextAreaField("Notes", render_kw={"placeholder" : "Enter any notes about the pet..."},validators=[Optional()])
 
-def url_or_upload(form, field):
-        if form.photo_url & form.photo_upload:
-            raise ValidationError("Please choose either a photo URL or upload.")
+
+class EditPetForm(FlaskForm):
+    """Form for editing pets."""
+
+    available = BooleanField("Available for adoption?")
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+    notes = TextAreaField("Notes", render_kw={
+                          "placeholder": "Enter any notes about the pet..."}, validators=[Optional()])
