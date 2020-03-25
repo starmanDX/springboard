0d3029acf36bae7e69f0abb116afd9544634b437
starmanDX <jamiecordrey@gmail.com>
2020-03-25 13:50:47 -0700

    modify python bcrypt auth demo files

--- demos/python/auth/bcrypt/app.py
+++ demos/python/auth/bcrypt/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db, User
-from forms import UserForm
+from models import connect_db, db, User, Tweet
+from forms import UserForm, TweetForm
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///auth_demo_db'
@@ -20,32 +21,73 @@ toolbar = DebugToolbarExtension(app)
 def home_page():
     return render_template('index.html')
 
-@app.route('/tweets')
+
+@app.route('/tweets', methods=['GET', 'POST'])
 def show_tweets():
+    """Show all tweets"""
+
     if "user_id" not in session:
-        flash('Please log in first.')
+        flash('Please log in first.', 'danger')
         return redirect('/login')
-    return render_template('tweets.html')
+
+    form = TweetForm()
+    all_tweets = Tweet.query.all()
+    if form.validate_on_submit():
+        text = form.text.data
+        new_tweet = Tweet(text=text, user_id=session['user_id'])
+        db.session.add(new_tweet)
+        db.session.commit()
+
+        flash('Successfully created tweet!', 'success')
+        return redirect('/tweets')
+    else:
+        return render_template('tweets.html', form=form, tweets=all_tweets)
+
+
+@app.route('/tweets/<int:id>', methods=['POST'])
+def delete_tweet(id):
+    """Delete tweet"""
+
+    if 'user_id' not in session:
+        flash('Please log in to do that!', 'danger')
+        return redirect('/login')
+
+    tweet = Tweet.query.get_or_404(id)
+
+    if tweet.user_id == session['user_id']:
+        db.session.delete(tweet)
+        db.session.commit()
+
+        flash("Successfully deleted tweet!", 'info')
+        return redirect('/tweets')
+
+    flash("You don't have permission do to that!", 'danger')
+    return redirect('/tweets')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_user():
     form = UserForm()
-    
+
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
 
         new_user = User.register(username, password)
         db.session.add(new_user)
-        db.session.commit()
-
+        try:
+            db.session.commit()
+        except IntegrityError:
+            form.username.errors.append('Username unavailable. Please choose another')
+            return render_template('register.html', form=form)
         session['user_id'] = new_user.id
 
-        flash('Welcome! You have successfully registered!')
+        flash('Welcome! You have successfully registered!', 'success')
 
         return redirect('/tweets')
     else:
         return render_template('register.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_user():
@@ -58,17 +100,18 @@ def login_user():
         user = User.authenticate(username, password)
 
         if user:
-            flash(f"Welcome back, {user.username}!")
+            flash(f"Welcome back, {user.username}!", 'info')
             session['user_id'] = user.id
             return redirect('/tweets')
         else:
             form.username.errors = ['Invalid username or password']
-    
+
     return render_template('login.html', form=form)
+
 
 @app.route('/logout', methods=['POST'])
 def logout_user():
     session.pop('user_id')
 
-    flash("You have been successfully logged out!")
+    flash("You have been successfully logged out!", 'info')
     return redirect('/')
--- demos/python/auth/bcrypt/models.py
+++ demos/python/auth/bcrypt/models.py
@@ -11,6 +11,16 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+class Tweet(db.Model):
+    """Tweet model"""
+
+    __tablename__ = 'tweets'
+
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='tweets')
 
 class User(db.Model):
 
