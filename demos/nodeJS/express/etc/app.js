580f63be265ca408e6469574b04860458dfe3c9c
starmanDX <jamiecordrey@gmail.com>
2020-04-30 18:41:07 -0700

    modify folder structure

--- demos/nodeJS/express/etc/app.js
+++ demos/nodeJS/express/etc/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
+const cookieParser = require('cookie-parser');
+
+// serve files in the 'public' directory as '/filename.ext'
+app.use(express.static("public"));
+// serve files in 'public/css' directory as '/css/filename.ext'
+app.use('/css', express.static("public/css"));
+app.use(cookieParser())
 
 // configure this to leave off .html from render files.
 app.set('view engine', 'html')
@@ -18,6 +25,13 @@ app.get('/', (req, res, next) => {
 app.get('/dogs/:name', (req, res, next) => {
     res.render('dog', {name: req.params.name});
 });
+
+// parses any cookies in the browser as JSON
+app.get('/showmecookies', (req, res, next) => {
+    // sets a cookie in the browser
+    res.cookie('isLoggedIn', 'true')
+    res.send(req.cookies);
+})
 
 
 app.listen(3000, () => {
