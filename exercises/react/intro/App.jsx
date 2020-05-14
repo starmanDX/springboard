324f64a6f31d6e677ed1b8968b8b250a66a30da1
starmanDX <jamiecordrey@gmail.com>
2020-05-14 16:29:18 -0700

    add react/intro exercise files

---
+++ exercises/react/intro/App.jsx
@@ -0,0 +1,8 @@
+const App = () => (
+  <div>
+    <FirstComponent />
+    <NamedComponent />
+  </div>
+);
+
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ exercises/react/intro/NamedComponent.jsx
@@ -0,0 +1,4 @@
+const NamedComponent = () => {
+  const name = "James";
+  return <p>My name is {name}</p>;
+};
