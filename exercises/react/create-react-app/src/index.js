a2598862d70a3282955305c3d2a60d158c430e05
starmanDX <jamiecordrey@gmail.com>
2020-05-17 01:00:10 -0700

    add react/create-react-app pokedex exercise

--- exercises/react/create-react-app/src/index.js
+++ exercises/react/create-react-app/src/index.js
@@ -1,12 +1,12 @@
-import { choice, remove } from "./helpers";
-import fruits from "./foods";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-let fruit = choice(fruits);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-console.log(`I'd like one ${fruit}, please`);
-console.log(`Here you go: ${fruit}`);
-console.log(`Delicious! May I have another?`);
-
-let remaining = remove(fruit, fruits);
-
-console.log(`I'm sorry, we're all out. We have ${remaining} left.`);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
