60a5344f94121b971c53d18be7f9269a4fd90b44
starmanDX <jamiecordrey@gmail.com>
2020-06-08 16:23:59 -0700

    modify react-router demo files

--- demos/react/react-router/src/App.js
+++ demos/react/react-router/src/App.js
@@ -1,33 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Home from "./Home";
 import Eat from "./Eat";
 import Drink from "./Drink";
+import NavBar from "./NavBar";
+
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-  const [page, setPage] = useState("home");
-
-  const goToPage = newPage => {
-    setPage(newPage);
-  }
-
-  const showPage = () => {
-    if (page === "home") return <Home />;
-    if (page === "eat") return <Eat />
-    if (page === "drink") return <Drink/>
-  }
-
   return (
-    <main>
-      <nav>
-        <a onClick={() => goToPage("drink")}>Drink</a>
-        <a onClick={() => goToPage("eat")}>Eat</a>
-        <a onClick={() => goToPage("home")}>Home</a>
-      </nav>
-      {showPage()}
-    </main>
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Route exact path="/drink">
+          <Drink />
+        </Route>
+        <Route exact path="/eat">
+          <Eat />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </BrowserRouter>
+    </div>
   );
 }
-// end
 
 export default App;
